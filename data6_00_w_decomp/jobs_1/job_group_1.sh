d3cf5dc7fc306139cc794a49fabfb34d32d18c0e
Claude-Guy Quimper <claude-guy.quimper@ift.ulava.ca>
2023-12-18 14:58:31 -0500

    Deleted empty directory

--- data6_00_w_decomp/jobs_1/job_group_1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --time=168:0:00
-#SBATCH --account=def-cquimper
-#SBATCH --mem-per-cpu=10G
-#SBATCH --array=1-510
-#SBATCH --output=data6_00_w_decomp/jobs_1/job_%a.out
-eval ./gen_paralle_conj.sh `awk "NR==$SLURM_ARRAY_TASK_ID" data6_00_w_decomp/jobs_1/threads_1.tex`
